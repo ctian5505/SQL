e23b248f507f5a8aef280b8cb4c9c6d8a5ab4617
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-09-10 22:18:50 +0800

    Create Week3_Project2_MS_SQL_Server_Masterclass.sql

---
+++ Week3_Project2_MS_SQL_Server_Masterclass.sql
@@ -0,0 +1,29 @@
+-- Activity # 2 - Creating a Schema
+/* 
+Q1 Create a new schema called "SalesSchema".
+Move the "FactInternetSales" table into the "SalesSchema" schema.
+*/
+
+
+-- Activity # 2 - Creating a Table
+/*
+Q2 Create a new table named "ProductReviews" with columns: ReviewID (INT), ProductID (INT), Rating (INT), ReviewText (VARCHAR), and ReviewDate (DATE).
+*/
+
+
+-- Activity # 2 - Adding Columns
+/*
+Q3 Add a column named “LinkedIn URL" of type VARCHAR(MAX) to the "DimEmployee" table.
+*/
+
+
+-- Activity # 2 - :Modifying Columns
+/*
+Q4 Modify the "LastName" column in the " DimEmployee" table to allow a maximum of 75 characters.
+*/
+
+
+-- Activity # 2 - :Deleting Columns
+/*
+Remove the “NameStyle" column from the “Employee" table.
+*/
