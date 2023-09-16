13d9e3ba0bd467fc637a2a64c89f132761168f12
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-09-16 12:24:51 +0800

    Update DSA_Final_Project.sql

--- DSA_Final_Project.sql
+++ DSA_Final_Project.sql
@@ -1,1 +1,9 @@
-SELECT
+--Project Phases and Tasks:
+  --Phase 1: Database Design (DDL)
+   --Task 1: Define the Database Schema | Description: Create the database schema for the library management system, defining 
+
+   --Task 2: Specify Keys and Constraints | Description: Define primary keys, foreign keys, constraints, and relationships between tables.
+
+
+
+
