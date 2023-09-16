41092dbaa906ad7b0119f266c75c92fb323bcbf2
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-09-16 18:28:45 +0800

    Update DSA_Final_Project.sql

--- DSA_Final_Project.sql
+++ DSA_Final_Project.sql
@@ -19,6 +19,24 @@ Genre NVARCHAR(150))
 
 
    --Task 2: Specify Keys and Constraints | Description: Define primary keys, foreign keys, constraints, and relationships between tables.
+	-- Defining Primary Key for books table
+ALTER TABLE Books
+ADD CONSTRAINT PK_Books PRIMARY KEY(BookID)
+
+	-- Defining Primary Key for books table
+ALTER TABLE Books
+ADD CONSTRAINT FK_Books_Genre FOREIGN KEY(GenreID)
+REFERENCES Genre(GenreID)
+
+
+
+-- Phase 2: Data Population (DML)
+	-- Task 3: Populate the Database with Sample Data | Description: Insert sample data into the database, including books, user information, and transactions.
+
+
+
+	-- Task 4: Implement Data Modification | Description: Create SQL scripts for updating, deleting, and modifying data as needed.
+
 
 
 
