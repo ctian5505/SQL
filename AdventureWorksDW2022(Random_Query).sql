a6563b335391f3ecd1681c5c8e966ce74bae4f90
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-10-09 21:01:48 +0800

    Update AdventureWorksDW2022(Random_Query).sql

--- AdventureWorksDW2022(Random_Query).sql
+++ AdventureWorksDW2022(Random_Query).sql
@@ -1,1 +1,11 @@
-sdadsad
+-- Calculate the total sales of each resellers and Rank it.
+SELECT 
+	FRS.ResellerKey,  DR.ResellerName, SUM(FRS.ExtendedAmount) Total_Sales, RANK() OVER (ORDER BY SUM(FRS.ExtendedAmount) DESC) AS Total_Sales_Rank
+ FROM 
+	FactResellerSales AS FRS
+LEFT JOIN
+	DimReseller AS DR
+ON
+	FRS.ResellerKey = DR.ResellerKey
+GROUP BY 
+	FRS.ResellerKey, DR.ResellerName
