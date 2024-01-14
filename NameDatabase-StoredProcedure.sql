5a8bc1c5bf3164fe38b2e13060d40bcb43a1f474
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-14 11:39:48 +0800

    Update NameDatabase-StoredProcedure.sql

--- NameDatabase-StoredProcedure.sql
+++ NameDatabase-StoredProcedure.sql
@@ -45,7 +45,7 @@ BEGIN
 
 -- Data Merging from source file to the database
 	MERGE NameDatabase AS target using Temp_table AS source on target.NameID = source.NameID WHEN MATCHED THEN 
-  --- This block of code is executed when the soucreID[Temp_table] and targetID[NameDatabase] has match, it simply means that the imformation is already existed and just need an update.
+	  --- This block of code is executed when the soucreID[Temp_table] and targetID[NameDatabase] has match, it simply means that the imformation is already existed and just need an update.
 	UPDATE
 	SET
 		target.NameID = source.NameID,
@@ -54,15 +54,15 @@ BEGIN
 		target.NickName = source.NickName,
 		Target.RegistrationDate = source.RegistrationDate
 
-  --- This block of code executed when the sourceid[Temp_table] didnt exist on targetID[NameDatabase], it means that this is a new data and need to be inserted
+  	--- This block of code executed when the sourceid[Temp_table] didnt exist on targetID[NameDatabase], it means that this is a new data and need to be inserted
 	WHEN NOT MATCHED by Target then
 	INSERT(
 	NameID,FirstName,LastName,NickName,RegistrationDate)
 	VALUES(
 	source.NameID,source.FirstName,source.LastName,source.NickName,source.RegistrationDate);
 
-- Dropping the source table to save space
+	-- Dropping the source table to save space
 	DROP TABLE Source$
-- Dropping the Temp table because it is no longer useful
+	-- Dropping the Temp table because it is no longer useful
 	DROP TABLE Temp_table
 END
